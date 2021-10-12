8a27ae4214328832182aa3830f93159bc25c59f0
Mohammad Ostadi <7m.ostadi5@gmail.com>
2021-10-12 15:43:11 +0330

    helper docs added.

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -5,6 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Job
+ *
+ * @property int $id
+ * @property string $title
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Schedule[] $schedules
+ * @property-read int|null $schedules_count
+ * @method static \Illuminate\Database\Eloquent\Builder|Job newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Job newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Job query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Job whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Job whereTitle($value)
+ * @mixin \Eloquent
+ */
 class Job extends Model
 {
     use HasFactory;
--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -5,6 +5,40 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Schedule
+ *
+ * @property int $id
+ * @property string $started_at
+ * @property string|null $ended_at
+ * @property int $worker_id
+ * @property int|null $job_id
+ * @property int|null $admin_id
+ * @property string|null $verified_at
+ * @property int|null $verified
+ * @property string|null $description
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\User|null $admin
+ * @property-read \App\Models\Job|null $job
+ * @property-read \App\Models\User $worker
+ * @method static \Illuminate\Database\Eloquent\Builder|Schedule newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Schedule newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Schedule query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Schedule verified()
+ * @method static \Illuminate\Database\Eloquent\Builder|Schedule whereAdminId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Schedule whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Schedule whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Schedule whereEndedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Schedule whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Schedule whereJobId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Schedule whereStartedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Schedule whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Schedule whereVerified($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Schedule whereVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Schedule whereWorkerId($value)
+ * @mixin \Eloquent
+ */
 class Schedule extends Model
 {
     use HasFactory;
